6175573fb0ef4c7b2d6ab0da278be0650224e9bb
RDD <RDD@rdd.io>
2022-09-20 08:50:07 +0200

    feat(fetch data): added call to retrieve data
    in json form

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 window.console.log('testing');
+
+let apiQuotes = [];
+
+function newQuote(){
+    
+}
+
+async function getQuotes(){
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        alert(error);
+    }
+}
+// On Load
+getQuotes();
